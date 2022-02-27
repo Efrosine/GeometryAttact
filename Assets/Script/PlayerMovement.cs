448ba5bef38dd7672baf68aad50efd490849f25b
Efrosine <efrosinegames@gmail.com>
2022-02-27 16:07:46 +0700

    Set Joystick
    Horizontal move + Jump

--- Assets/Script/PlayerMovement.cs
+++ Assets/Script/PlayerMovement.cs
@@ -4,19 +4,53 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-   
+
+    [SerializeField] private float MovementSoeed;
+    [SerializeField] private float JumpForce;
+    [SerializeField] private LayerMask jumableGround;
+
+    [SerializeField] private Joystick joystick;
+
+    private Rigidbody2D rigidbody;
+    private BoxCollider2D coll;
+
     void Start()
     {
-        
+        rigidbody = GetComponent<Rigidbody2D>();
+        coll = GetComponent<BoxCollider2D>();
     }
 
- 
+
     void Update()
     {
-        for(int i = 0; i < Input.touchCount; i++)
+       
+    }
+
+    private void FixedUpdate()
+    {
+        var movement =0f;
+        if(joystick.Horizontal >= .2f)
         {
-            Vector3 touchPost = Camera.main.ScreenToWorldPoint(Input.touches[i].position);
-            Debug.DrawLine(Vector3.zero, touchPost, Color.red);
+            movement = 1f;
+        }else if(joystick.Horizontal <= -.2f)
+        {
+            movement = -1f;
         }
+        else
+        {
+            movement = 0f;
+        }
+
+        if(joystick.Vertical >= .5f && isGrounded())
+        {
+            rigidbody.AddForce(new Vector2(0, JumpForce), ForceMode2D.Impulse);
+        }
+      
+        transform.position += new Vector3(movement, 0, 0) * Time.deltaTime * MovementSoeed;
+    }
+
+    bool isGrounded()
+    {
+        return Physics2D.BoxCast(coll.bounds.center, coll.bounds.size, 0f, Vector2.down, .1f, jumableGround);
     }
 }
